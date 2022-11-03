e7b8747370a15cb7ea836efda1a229d340fdf188
Thomas Marchand <thomas.marchand@tuta.io>
2022-11-03 14:10:37 +0100

    remove nvm because it was too slow

--- configs/main.nix
+++ configs/main.nix
@@ -28,9 +28,6 @@
         export PATH=$PATH:~/.protostar/dist/protostar/;
         export PATH=$PATH:/opt/homebrew/bin;
         export PATH=$PATH:/usr/local/bin;
-        export NVM_DIR="$HOME/.nvm";
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
       '';
       plugins = [
         {
