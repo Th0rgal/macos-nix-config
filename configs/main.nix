eac058330dd0dfa8176957382785d6c0d67515ee
Thomas Marchand <thomas.marchand@tuta.io>
2025-11-20 16:21:09 +0000

    feat: node@24

--- configs/main.nix
+++ configs/main.nix
@@ -46,6 +46,7 @@
         export PATH=$PATH:~/.starkli/bin;
         export PATH=$PATH:/opt/homebrew/opt/openssl@1.1/bin;
         export PATH=$PATH:~/.bun/install/global/node_modules/supabase/bin;
+        export PATH="/opt/homebrew/opt/node@24/bin:$PATH";
         # export PATH=$PATH:/Users/thomas/Library/Application\ Support/org.dfinity.dfx/bin;
 
         # NVM configuration
