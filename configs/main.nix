03f5f8015577c6a50c7a236bb691cfe9380d5d03
Thomas Marchand <thomas.marchand@tuta.io>
2026-01-01 06:56:02 +0100

    fix: path

--- configs/main.nix
+++ configs/main.nix
@@ -48,6 +48,8 @@
         export PATH=$PATH:/opt/homebrew/opt/libpq/bin;
         export PATH=$PATH:~/.bun/install/global/node_modules/supabase/bin;
         export PATH="/opt/homebrew/opt/node@24/bin:$PATH";
+        export PATH="$HOME/Library/Python/3.9/bin:$PATH";
+
         # export PATH=$PATH:/Users/thomas/Library/Application\ Support/org.dfinity.dfx/bin;
 
         # NVM configuration
